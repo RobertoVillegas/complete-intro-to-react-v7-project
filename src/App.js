28f233f5a4705db3dc2bfc3bb434c7fa492bf954
Roberto Villegas <hola@robertovillegas.com>
2022-07-09 22:44:22 -0500

    First App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { render } from "react-dom";
+import { StrictMode } from "react";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,31 @@
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "https://pet-images.dev-apis.com/pets/none.jpg";
+
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
+  );
+};
+
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
+
+export default Pet;
